9ea068aa613ecec72292ffa134d1affadb5d1c17
amrit-choudhary <albela.amrit@gmail.com>
2018-11-02 18:38:40 +0530

    pug

--- myapp/helloworld/app.js
+++ myapp/helloworld/app.js
@@ -7,6 +7,8 @@ var serveFavicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
